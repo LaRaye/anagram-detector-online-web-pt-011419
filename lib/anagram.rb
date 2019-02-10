a94ad0b5cb2a4774ffe4918850fc7775681c6b87
LaRaye Johnson <lnj2105@gmail.com>
2019-02-10 17:25:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+    match_arr = []
+    array.each do |possible|
+      pos_arr = possible.split(" ")
+      word_arr = @word.split(" ")
+      
+      if word_arr == pos_arr
+        match_arr << possible
+      end
+    end
+    match_arr
+  end 
+
+  
+
+  
+
+
+
+end
